ae6a06195e909a4aeef7dd512f923b42be29627f
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-08-01 15:34:00 +0900

    바이러스_(런타임 에러)
    - BFS를 이용한 문제 풀이

---
+++ src/week6/A_2606.java
@@ -0,0 +1,62 @@
+package week6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class A_2606 {
+    //BFS로 문제 풀이
+    static ArrayList<Integer>[] linked;
+    static boolean[] infection;
+
+    static void bfs(int computer){
+        int count=0;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(computer);
+        infection[computer] = true;
+        while(!q.isEmpty()){
+            int x = q.remove();
+            System.out.print(x + " ");
+            for(int y : linked[x]){
+                if(!infection[y]){
+                    infection[y] = true;
+                    q.add(y);
+                    count++;
+                }
+            }
+        }
+        System.out.println(" ");
+        System.out.print(count);
+    }
+
+     public static void main(String[] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         StringTokenizer st = new StringTokenizer(br.readLine());
+
+         int computers = Integer.parseInt(st.nextToken());
+         int linkedComputers = Integer.parseInt(br.readLine());
+
+         linked = new ArrayList[computers+1];
+
+         for(int i=1; i<=computers; i++){
+             linked[i] = new ArrayList<Integer>();
+         }
+         for(int i=1; i<=linkedComputers; ++i){
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                linked[a].add(b);
+                linked[b].add(a);
+         }
+
+         for(int i=1; i<=linkedComputers; ++i){
+             Collections.sort(linked[i]);
+         }
+
+        infection = new boolean[computers+1];
+         bfs(1);
+
+    }
+}
