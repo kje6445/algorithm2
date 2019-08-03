a08a1d77de87690e71fd93773768be7536d31fcf
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-08-04 06:58:47 +0900

    바이러스_(런타임 오류 해결)
    - Collections.sort()가 여러번 돌아가서 런타임 오류가 발생하였었다. 때문에 BFS 코드를 수정하여 정렬이 필요없도록 하였다.

--- src/week6/A_2606.java
+++ src/week6/A_2606.java
@@ -6,17 +6,37 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 
-/*
 public class A_2606 {
     //BFS로 문제 풀이
-    static ArrayList<Integer>[] linked;
-    static boolean[] infection;
 
-    static void bfs(int computer){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int computers = Integer.parseInt(st.nextToken());
+        int linkedComputers = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer>[] linked = new ArrayList[computers+1];
+        boolean[] infection = new boolean[computers+1];
+
+        for(int i=1; i<=computers; i++){
+            linked[i] = new ArrayList<>();
+        }
+        for(int i=1; i<=linkedComputers; ++i){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            linked[a].add(b);
+            linked[b].add(a);
+        }
+
+
         int count=0;
+
         Queue<Integer> q = new LinkedList<>();
-        q.add(computer);
-        infection[computer] = true;
+        q.add(1);
+
         while(!q.isEmpty()){
             int x = q.remove();
             System.out.print(x + " ");
@@ -24,42 +44,15 @@ public class A_2606 {
                 if(!infection[y]){
                     infection[y] = true;
                     q.add(y);
-                    count++;
                 }
             }
         }
-        System.out.println(" ");
-        System.out.print(count);
-    }
+        for(int i=2;i<=infection.length-1; i++)
+            if(infection[i])
+                count++;
 
-     public static void main(String[] args) throws IOException {
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-         StringTokenizer st = new StringTokenizer(br.readLine());
-
-         int computers = Integer.parseInt(st.nextToken());
-         int linkedComputers = Integer.parseInt(br.readLine());
-
-         linked = new ArrayList[computers+1];
-
-         for(int i=1; i<=computers; i++){
-             linked[i] = new ArrayList<Integer>();
-         }
-         for(int i=1; i<=linkedComputers; ++i){
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-
-                linked[a].add(b);
-                linked[b].add(a);
-         }
-
-         for(int i=1; i<=linkedComputers; ++i){
-             Collections.sort(linked[i]);
-         }
-
-        infection = new boolean[computers+1];
-         bfs(1);
-
+        System.out.println();
+        System.out.println(count);
     }
 }
-*/
+
