5f4c1ac8bc8d31a9c95bbea41f85aa1dc07acb34
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-08-28 18:59:35 +0900

    1,2,3 더하기
    - 동적프로그래밍을 구현하여 재귀적용법으로 구현
    - 규칙을 찾는 것이 관건

--- src/week7/B_9095.java
+++ src/week7/B_9095.java
@@ -1,7 +1,26 @@
 package week7;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class B_9095 {
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int[] numbers = new int[100];
+		numbers[1] = 1; //1을 넣을 경우 1가지(1)
+		numbers[2] = 2; //2를 넣을 경우 2가지(1+1, 2)
+		numbers[3] = 4; //3을 넣을 경우 3가지(1+1+1, 1+2, 2+1, 3)
+
+		for (int i = 0; i < n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			for (int j = 4; j <= num; j++) {
+				numbers[j] = numbers[j - 1] + numbers[j - 2] + numbers[j - 3];
+			}
+			System.out.println(numbers[num]);
+		}
 
 	}
 }
