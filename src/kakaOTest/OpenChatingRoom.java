3027172d1dd1f1d87aacc6317144b52d3324a1c9
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-09-01 22:47:17 +0900

    오픈채팅방_(문제 해결 요함)
    - record 배열로 메소드를 만들어서 문제를 풀어야한다.

---
+++ src/kakaOTest/OpenChatingRoom.java
@@ -0,0 +1,23 @@
+package kakaOTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class OpenChatingRoom {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in);
+
+		int[] record = new int[100000];
+
+	}
+
+	public String[] solution(String[] record){
+		String[] answer={};
+		//record의 문자열에서 1. Enter, 2. Leave, 3.Change 등의 3개로 나눠서 구분한다.
+		//1 -> 들어왔습니다. 2 ->  나갔습니다. -> 모두 5글자
+		//~가 나갔습니다의 ~를 찾는것은 uidID로 찾는 것이다.
+		
+		return answer;
+	}
+}
