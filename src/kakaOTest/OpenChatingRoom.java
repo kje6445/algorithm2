fa991ffad5ebd82b2d58f1c1f256cf492503e45c
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-09-02 23:27:25 +0900

    오픈 채팅_(문제 해결 요함)
    - HashMap을 이용해 문제를 해결 요함

--- src/kakaOTest/OpenChatingRoom.java
+++ src/kakaOTest/OpenChatingRoom.java
@@ -1,23 +1,35 @@
 package kakaOTest;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+
+import java.util.HashMap;
 
 public class OpenChatingRoom {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in);
+	public static void main(String[] args){
 
-		int[] record = new int[100000];
+		String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"};
 
+		String[] solution = solution(record);
+
+		for (int i = 0; i < solution.length; i++) {
+			System.out.print(solution[i]);
+		}
 	}
 
-	public String[] solution(String[] record){
+	public static String[] solution(String[] record){
 		String[] answer={};
-		//record의 문자열에서 1. Enter, 2. Leave, 3.Change 등의 3개로 나눠서 구분한다.
-		//1 -> 들어왔습니다. 2 ->  나갔습니다. -> 모두 5글자
-		//~가 나갔습니다의 ~를 찾는것은 uidID로 찾는 것이다.
-		
+		String[] StoreMessages = {"Enter","Leave","Change"};
+		HashMap<String, String[]> messageCheck = new HashMap<>();
+
+		for(int i=0; i<record.length; i++){
+			String[] testMessages = record[i].split(" ");
+			messageCheck.put(testMessages[0], new String[]{testMessages[1], testMessages[2]});
+		}
+
+		if(StoreMessages[0].equals(messageCheck.get())){
+			System.out.println(messageCheck.get()+"님이 들어왔습니다.");
+		}
+		//record 값을 마지막 배열까지 입력 받고 나서 알맞게 출력하도록한다.
 		return answer;
 	}
 }
