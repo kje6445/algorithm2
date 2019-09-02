469a2a464d0123219a39e97d872222b9dbd5b623
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-09-02 19:39:09 +0900

    실패율_(문제 해결 요함)
    - 문제의 접근을 거의 다 메소드를 통해 하였으나 문제에 0만 출력이 되는 오류 발생, 해결 요함

--- src/kakaOTest/FailureRatio.java
+++ src/kakaOTest/FailureRatio.java
@@ -3,36 +3,59 @@ package kakaOTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class FailureRatio {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		//실패율 -> 스테이지에 도달했지만 클리어 못한 플레이어의 수/ 스테이지에 도달한 플레이어수
-		int N = Integer.parseInt(br.readLine()); //스테이지 개수 1~500
+		int N = 5; //스테이지 개수 1~500
 
-		int[] stages = new int[N+1]; //현재 멈취있는 스테이지의 번호가 담긴 배열, N+1은 마지막까지 클리어한 사람
+		int[] stages = {2, 1, 2, 6, 2, 4, 3, 3}; //현재 멈취있는 스테이지의 번호가 담긴 배열, N+1은 마지막까지 클리어한 사람
 		//실패율이 같은 스테이지가 있다면 작은 번호부터 오도록. 스테이지에 도달한 유저 없는 경우
 		//실패율 0
 
-		for(int i=0; i<stages.length; i++){
+		/*for (int i = 0; i < stages.length; i++) {
 			stages[i] = Integer.parseInt(br.readLine());
 		}
-
+*/
 		int[] solution = solution(N, stages);
 
-		System.out.println(solution);
+		for (int i = 0; i < solution.length; i++) {
+			System.out.println(solution[i]);
+		}
 	}
-	public static int[] solution(int N, int[] stages){
-		int[] count = new int[N+1];
-		for(int i=0; i<stages.length; i++){
-			switch (stages[i]){
-				case 1:
-					++count[0];
-				case 2:
-					++count[1];
+
+	public static int[] solution(int N, int[] stages) {
+		int[] count = new int[N + 1];
+		for (int i = 0; i < stages.length; i++) {
+			int num = 0;
+			stages[i] = num;
+			if (num < N) {
+				++count[num];
+			} else {
+				++count[0];
 			}
 		}
-		int[] answer = stages;
+		double[] failureRatio = new double[N + 1];
+		int sum = stages.length;
+		int lesSum = 0;
+		for (int i = 1; i < count.length; i++) {
+			failureRatio[i] = (double) count[i] / (sum - lesSum);
+			lesSum += count[i];
+		}
+		int[] answer = new int[N];
+		double max = failureRatio[1];
+		int result = 0;
+		for (int i = 1; i < failureRatio.length; i++) {
+			if (max < failureRatio[i]) {
+				max = failureRatio[i];
+				result = i;
+			}
+			if (i == N + 1) {
+				failureRatio[result] = -1;
+			}
+		}
 		return answer;
 	}
 }
