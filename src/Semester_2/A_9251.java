a175e5518ad2bc68b9c6db5ef11095fda4c28836
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-09-18 23:58:50 +0900

    LCS_(해결 요함)

---
+++ src/Semester_2/A_9251.java
@@ -0,0 +1,18 @@
+package Semester_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A_9251 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		String[] firstArr = st.nextToken().split("");
+		String[] secondtArr = st.nextToken().split("");
+
+		//아스키 코드로 문자 비교해서 저장해 순서대로 out 하기
+	}
+}
