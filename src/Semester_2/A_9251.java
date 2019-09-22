97a136c6fc87fc9b6964e75081c5c7018a520659
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-09-22 16:00:05 +0900

    LCS
    - 부분 수열을 통한 문제 풀이

--- src/Semester_2/A_9251.java
+++ src/Semester_2/A_9251.java
@@ -1,18 +1,33 @@
 package Semester_2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class A_9251 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-		String[] firstArr = st.nextToken().split("");
-		String[] secondtArr = st.nextToken().split("");
+		String st1 = sc.nextLine();
+		String st2 = sc.nextLine();
+		sc.close();
+		;
 
-		//아스키 코드로 문자 비교해서 저장해 순서대로 out 하기
+		int[][] dp = new int[st1.length() + 1][st2.length() + 1];
+
+		for (int x = 1; x <= st1.length(); x++) {
+
+			char c1 = st1.charAt(x - 1);
+
+			for (int y = 1; y <= st2.length(); y++) {
+				char c2 = st2.charAt(y - 1);
+
+				if (c1 == c2) { //값이 같으면 좌측 대각선 위의 값 +1
+					dp[x][y] = dp[x - 1][y - 1] + 1;
+				} else { //값이 다르면 좌측(x-1), 상단(y-1)의 값 중 더 큰 값
+					dp[x][y] = Math.max(dp[x - 1][y], dp[x][y - 1]);
+				}
+			}
+		}
+
+		System.out.println(dp[st1.length()][st2.length()]);
 	}
 }
