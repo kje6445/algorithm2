62d188cd0b73b33eccb9aa31832f540eabf322c2
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-12-10 21:11:59 +0900

    상근이의 여행(미완성)
    - 최소 편집거리를 이용

---
+++ src/WinterBase1/A_9372.java
@@ -0,0 +1,18 @@
+package WinterBase1;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+//최소 편집거리 문제
+public class A_9372 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int testcase = sc.nextInt();
+		StringTokenizer st = new StringTokenizer(sc.nextLine());
+		int nation = Integer.parseInt(st.nextToken());
+		int plane = Integer.parseInt(st.nextToken());
+
+
+	}
+}
