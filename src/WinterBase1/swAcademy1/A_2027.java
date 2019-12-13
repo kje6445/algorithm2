9cdec963de472f87dfb60bfe60b2b9b44edd622c
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-12-13 23:21:34 +0900

    (SW)대각선 출력하기

---
+++ src/WinterBase1/swAcademy1/A_2027.java
@@ -0,0 +1,12 @@
+package WinterBase1.swAcademy1;
+
+//대각선 출력하기
+public class A_2027 {
+	public static void main(String[] args)  {
+		System.out.println("#++++");
+		System.out.println("+#+++");
+		System.out.println("#++++");
+		System.out.println("+++#+");
+		System.out.println("++++#");
+	}
+}
