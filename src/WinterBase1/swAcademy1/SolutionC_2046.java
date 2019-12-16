92836466e24cd0d44553f04774ba036ed73a033c
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-12-16 22:06:29 +0900

    (SW)스탬프 찍기

---
+++ src/WinterBase1/swAcademy1/SolutionC_2046.java
@@ -0,0 +1,15 @@
+package WinterBase1.swAcademy1;
+
+import java.util.Scanner;
+
+// 스탬프 찍기
+public class SolutionC_2046 {
+	public static void main(String[] args)  {
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+
+		for(int i=-0; i<num; i++)
+			System.out.printf("#");
+	}
+}
