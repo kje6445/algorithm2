4692def6ecf793423ea2e2c4a45622b075dc3407
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-12-17 18:21:41 +0900

    (SW)N줄덧셈

---
+++ src/WinterBase1/swAcademy1/SolutionG_2025.java
@@ -0,0 +1,19 @@
+package WinterBase1.swAcademy1;
+
+import java.util.Scanner;
+
+//N줄덧셈
+public class SolutionG_2025 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		int sum =0;
+
+		for(int i=1; i<=num; i++){
+			sum+=i;
+		}
+
+		System.out.println(sum);
+	}
+}
