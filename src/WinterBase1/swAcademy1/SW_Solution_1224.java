525c4cf944964e6ab7d6bc87d3a259cfdf98a0db
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-12-18 23:09:03 +0900

    (sw)[S/W 문제해결 기본] 6일차 - 계산기3(오류 포함)
    계산을 후위 표기식으로 바꾸어 계산하는 프로그래밍

---
+++ src/WinterBase1/swAcademy1/SW_Solution_1224.java
@@ -0,0 +1,28 @@
+package WinterBase1.swAcademy1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+//[S/W 문제해결 기본] 6일차 - 계산기3
+//계산을 후위 표기식으로 바꾸어 계산하는 프로그래밍
+
+public class SW_Solution_1224 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for(int i=1; i<=10; i++) {
+			int testCase = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			Stack first = new Stack<>();
+			Stack second = new Stack();
+
+
+
+		}
+
+	}
+}
