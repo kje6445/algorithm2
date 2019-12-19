1cecc3fae379791978f6e9537a13d6b80255f963
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-12-20 03:21:06 +0900

    (sw)간단한 소인수분해

--- src/WinterBase1/swAcademy1/Solution_1945.java
+++ src/WinterBase1/swAcademy1/Solution_1945.java
@@ -5,32 +5,42 @@ import java.util.Scanner;
 
 //간단한 소인수분해
 public class Solution_1945 {
-	public static void main(String[] args)  throws IOException {
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 
 		int testcase = sc.nextInt();
 
-		for(int i=1; i<=testcase; i++){
+		for (int i = 1; i <= testcase; i++) {
 			int num = sc.nextInt();
-			int a=0; int b=0; int c=0; int d=0; int e=0;
+			int a = 0;
+			int b = 0;
+			int c = 0;
+			int d = 0;
+			int e = 0;
 
-			while(num%2!=0){
-				if(num%2==0)
+			while (num % 2 == 0) {
+				num = num / 2;
 				a++;
 			}
-			while(num%3!=0){
+
+			while (num % 3 == 0) {
+				num = num / 3;
 				b++;
 			}
-			while(num%5!=0){
+			while (num % 5 == 0) {
+				num = num / 5;
 				c++;
 			}
-			while(num%7!=0){
+			while (num % 7 == 0) {
+				num = num / 7;
 				d++;
 			}
-			while(num%11!=0){
+			while (num % 11 == 0) {
+				num = num / 11;
 				e++;
 			}
-			System.out.println("#"+i+" "+a+" "+b+" "+" "+c+" "+d+" "+e);
+			System.out.println("#" + i + " " + a + " " + b + " " + c + " " + d + " " + e);
 		}
 	}
 }
+
