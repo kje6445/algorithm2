312f13db52e15923b43a14de2635a5e5974fb092
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-12-20 12:09:30 +0900

    (sw) Base64 Decoder_(미완성)

--- src/WinterBase1/swAcademy1/Solution_1928.java
+++ src/WinterBase1/swAcademy1/Solution_1928.java
@@ -1,9 +1,19 @@
 package WinterBase1.swAcademy1;
 
 import java.io.IOException;
+import java.util.Scanner;
+
 // Base64 Decoder
 public class Solution_1928 {
 	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
 
+		int T = sc.nextInt();
+
+		for(int i=0; i<T; i++){
+			String encoding = sc.next();
+
+			System.out.println("#"+(i+1)+" ");
+		}
 	}
 }
