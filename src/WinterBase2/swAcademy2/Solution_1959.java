9aeae68bee404c51f7307688350ccb3dba24442b
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-12-23 03:53:31 +0900

    (sw) 두 개의 숫자열_(오류포함)

---
+++ src/WinterBase2/swAcademy2/Solution_1959.java
@@ -0,0 +1,19 @@
+package WinterBase2.swAcademy2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_1959 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int testCase = Integer.parseInt(br.readLine());
+
+		for(int i=1; i<=testCase; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			//-을 어떻게 처리 할 것인지에 대한 고민도 필요하다.
+		}
+	}
+}
