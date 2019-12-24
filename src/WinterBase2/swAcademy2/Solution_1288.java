83d49c2854f97dacb67f8cda25704b52e7d8549c
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-12-25 02:44:57 +0900

    (sw) 새로운 불면증 치료법_(오류포함)

---
+++ src/WinterBase2/swAcademy2/Solution_1288.java
@@ -0,0 +1,18 @@
+package WinterBase2.swAcademy2;
+
+import java.util.Scanner;
+
+//새로운 불면증 치료법
+public class Solution_1288 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		int testCase = sc.nextInt();
+
+		for(int i=1; i<=testCase; i++){
+			int num = sc.nextInt();
+			
+			System.out.printf("#"+i);
+		}
+	}
+}
