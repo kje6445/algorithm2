e2c4015940a691ad50d0bd885f2172eaee34c06c
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-01-03 02:18:39 +0900

    (sw) 원재의 메모리 복구하기_(오류 포함)
    - 10개의 테스트 케이스 중에 3개만 맞춤, 예외처리 과정이 필요

--- src/WinterBase1/swAcademy1/Solution_1289.java
+++ src/WinterBase1/swAcademy1/Solution_1289.java
@@ -1,34 +1,39 @@
 package WinterBase1.swAcademy1;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 //원재의 메모리 복구하기
 public class Solution_1289 {
 	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
+		//원래의 값이 주어지고 초기화된 값이 0밖에 없을 때
+		//다시 원래의 값으로 돌아가는 방법을 구하시오.
 
-		int T = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		for(int i=0; i<T; i++){
+		int testCase = Integer.parseInt(br.readLine());
 
-			char[] memoryArr = sc.nextLine().toCharArray();
+		for(int i=1; i<=testCase; i++){
 
-			int answer = 0;
-			for(int k=0; k<memoryArr.length; k++){
-				if(memoryArr[0] =='0'){
-					if(memoryArr[k] == '1') {
-						answer = memoryArr.length - k;
-						break;
-					}
-				}else{
-					if(memoryArr[k] == '0') {
-						answer = k-1;
-						break;
-					}
+			String arr = br.readLine();
+			char[] memoryArr = arr.toCharArray();
+			/*for (int k = 0; k < memoryArr.length; k++) {
+				System.out.printf(" "+memoryArr[k]);
+			}*/
+			int count = 0;
+			//중간에 변조가 많이 일어나면 그 횟수대로 count해서 세어주면 된다.
+			char first = memoryArr[0];
+			for(int p=1; p<memoryArr.length-1; p++){
+				if((memoryArr[p] != memoryArr[p+1]) || (memoryArr[1] != first)){
+					++count;
 				}
+		//		System.out.println("p "+p);
 			}
-			System.out.println("#"+(i+1)+" "+answer);
+		//	System.out.println("count "+count);
+			if(memoryArr[1] != first)
+				++count;
+			System.out.println("#"+i+" "+count);
 		}
 
 	}
