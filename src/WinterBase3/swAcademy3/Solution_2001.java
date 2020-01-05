b655979ffac8e5a7fb14291407fb451250792cf0
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-01-06 00:52:26 +0900

    (sw) 파리 퇴치_(문제 해결)

--- src/WinterBase3/swAcademy3/Solution_2001.java
+++ src/WinterBase3/swAcademy3/Solution_2001.java
@@ -12,31 +12,43 @@ public class Solution_2001 {
 
 		int testCase = Integer.parseInt(br.readLine());
 
-		for(int i=1; i<=testCase; i++) {
+		for (int i = 1; i <= testCase; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			int m = Integer.parseInt(st.nextToken());
 
 			int[][] matrixN = new int[n][n];
 
-			for(int p=0; p<n; p++){
-				String[] arr = br.readLine().split("");
-				for(int k=0; p<n; k++){
+			for (int p = 0; p < n; p++) {
+				st = new StringTokenizer(br.readLine());
+				String[] arr = new String[st.countTokens()];
+
+				for (int k = 0; k < n; k++)
+					arr[k] = st.nextToken();
+
+				for (int k = 0; k < n; k++) {
 					matrixN[p][k] = Integer.parseInt(arr[k]);
 				}
 			}
 
-			int sum =0;
-			int maxSum=0;
+			int sum, max = 0;
 
-			for(int p=0; p<m; p++){
-				for(int k=0; k<m; k++){
-					sum+=matrixN[p][k];
+			for(int p=0; p<n-m+1; p++) {
+				for(int j=0; j<n-m+1; j++) {
+					sum = 0;
+					for(int k=0; k<m; k++) {
+						for(int l=0; l<m; l++) {
+							sum += matrixN[p+k][j+l];
+						}
+					}
+					if( sum > max ) max = sum;
 				}
 			}
-			//matrixM의 값에 대해서 sum을 maxSum와 비교해서 큰걸 적는다.
 
-			System.out.println("#"+i+" "+maxSum);
+
+
+			System.out.println("#" + i + " " + max);
 		}
 	}
+
 }
