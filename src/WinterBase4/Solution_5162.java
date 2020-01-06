a26acf559c680f6e1e03826582f69d8aec45e0fb
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-01-07 07:36:40 +0900

    (sw) 파스칼의 삼각형

---
+++ src/WinterBase4/Solution_5162.java
@@ -0,0 +1,33 @@
+package WinterBase4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_5162 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int testCase = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= testCase; i++) {
+			String[] arr = br.readLine().split(" ");
+			int A=0, B = 0, C=0;
+
+			for (int p = 0; p < 3; p++) {
+				if (p == 0) {
+					A = Integer.parseInt(arr[p]);
+				} else if (p == 1) {
+					B = Integer.parseInt(arr[p]);
+				} else {
+					C = Integer.parseInt(arr[p]);
+				}
+			}
+
+			int check =C/Math.min(A,B);
+
+			System.out.println("#" + i + " "+check);
+		}
+	}
+}
