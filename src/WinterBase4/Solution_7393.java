c436a3ffe06d3473c339b117f88d88097f2c4245
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-01-08 22:54:07 +0900

    (sw) 대규의 팬덤활동_(오류 포함)
    - 문제 해석 오류

---
+++ src/WinterBase4/Solution_7393.java
@@ -0,0 +1,19 @@
+package WinterBase4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//대규의 팬덤활동
+public class Solution_7393 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int testCase = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= testCase; i++) {
+
+			System.out.println("#"+i);
+		}
+	}
+}
