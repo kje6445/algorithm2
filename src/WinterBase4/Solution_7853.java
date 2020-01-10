004ef8224d9f967bb089d4f5a95cb752da2287c2
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-01-10 20:44:58 +0900

    (sw) 오타_(문제이해_오류)
    - 완벽하게 문제가 이해가 되지 않아 풀이 실패

---
+++ src/WinterBase4/Solution_7853.java
@@ -0,0 +1,61 @@
+package WinterBase4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//오타
+public class Solution_7853 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int testCase = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= testCase; i++) {
+			String wordArr = br.readLine();
+			char[] splitWord = wordArr.toCharArray();
+			int[] checkNum = new int[splitWord.length];
+
+			for (int p = 0; p < checkNum.length; p++) {
+				checkNum[p] = 0;
+			}
+			int sum = 1;
+	//		for (int p = 0; p < checkNum.length; p++) {
+//				System.out.printf( splitWord[p]+" ");
+//			}
+
+			if (splitWord.length != 1) {
+				for (int p = 0; p < splitWord.length; p++) {
+					if (p == 0) {
+						if (splitWord[p] == splitWord[p + 1]) {
+							checkNum[p] = 1;
+						}else{
+							checkNum[p] = 2;
+						}
+					} else if (p == splitWord.length - 1) {
+						if (splitWord[p] == splitWord[p-1]) {
+							checkNum[p] = 1;
+						}else{
+							checkNum[p] = 2;
+						}
+					} else {
+						if ((splitWord[p] == splitWord[p - 1]) && (splitWord[p] == splitWord[p + 1])) {
+							checkNum[p] = 1;
+						} else if ((splitWord[p] != splitWord[p - 1]) && (splitWord[p] != splitWord[p + 1])&&(splitWord[p+1] != splitWord[p - 1])) {
+							checkNum[p] = 3;
+						} else {
+							checkNum[p] = 2;
+						}
+					}
+				}
+
+				for (int p = 0; p < splitWord.length; p++) {
+					System.out.println("checkNum[p] "+checkNum[p]);
+					sum = checkNum[p] * sum;
+				}
+			}
+			System.out.println("#" + i + " " + sum);
+		}
+	}
+}
