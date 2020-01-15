0a5058033a8771336322623a520b88a5376dc26c
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-01-16 00:58:17 +0900

    (sw) 다솔이의 월급 상자_(문제 해결)

--- src/WinterBase4/Solution_6692.java
+++ src/WinterBase4/Solution_6692.java
@@ -3,7 +3,6 @@ package WinterBase4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 //다솔이의 월급 상자
 public class Solution_6692 {
@@ -12,16 +11,15 @@ public class Solution_6692 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int testCases = Integer.parseInt(br.readLine());
+
 		for (int i = 1; i <= testCases; i++) {
 			int T = Integer.parseInt(br.readLine());
-			double[][] persnet= new double[T][2];
-
-			for(int p =0; p<=T; p++){
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				persnet[p][0] = Double.parseDouble(st.nextToken());
-				persnet[p][1] = Double.parseDouble(st.nextToken());
+			double result = 0.0;
+			for (int p = 1; p <= T; p++) {
+				String[] value = br.readLine().split(" ");
+				result += Double.parseDouble(value[0]) * Double.parseDouble(value[1]);
 			}
-			System.out.printf("#"+i);
+			System.out.printf("#"+i+" "+result+"\n");
 		}
 	}
 }
