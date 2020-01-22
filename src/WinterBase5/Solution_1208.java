be0033630728439f67cdd5778dea91be2eca44cb
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-01-22 23:00:53 +0900

    (sw) [S/W 문제해결 기본] 1일차 - Flatten_(문제 해결)
    - 문제를 잘 읽어야 하며 일단 문제를 작은 수에서 큰 수로 정렬부터 할 것

--- src/WinterBase5/Solution_1208.java
+++ src/WinterBase5/Solution_1208.java
@@ -3,6 +3,7 @@ package WinterBase5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 //[S/W 문제해결 기본] 1일차 - Flatten
@@ -11,19 +12,23 @@ public class Solution_1208 {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int testCase = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= 10; i++) {
 
-		for (int i = 1; i <= testCase; i++) {
 			int nums = Integer.parseInt(br.readLine());
-
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int[] arr = new int[nums];
+			int[] arr = new int[100];
 
-			for(int p=0; p<nums; p++){
+			for (int p = 0; p < 100; p++) {
 				arr[p] = Integer.parseInt(st.nextToken());
 			}
 
-
+			Arrays.sort(arr);
+			for (int k = 0; k < nums; k++) {
+				arr[0]++;
+				arr[99]--;
+				Arrays.sort(arr);
+			}
+			System.out.println("#" + i + " " + (arr[99] - arr[0]));
 		}
 	}
 }
