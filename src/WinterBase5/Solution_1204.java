d6b70fb7edefe89e75f68fd0ea76af9d5faf9cfa
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-01-26 20:01:28 +0900

    (sw) [S/W 문제해결 기본] 1일차 - 최빈수 구하기_(문제 해결 요망)

---
+++ src/WinterBase5/Solution_1204.java
@@ -0,0 +1,45 @@
+package WinterBase5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//[S/W 문제해결 기본] 1일차 - 최빈수 구하기
+public class Solution_1204 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int testCase = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= testCase; i++) {
+			int caseNum = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int[] arr = new int[1000];
+			int[] idxArr = new int[101];
+
+			for (int p = 0; p <= 100; p++) {
+				idxArr[p] = 0;
+			}
+
+			for (int p = 0; p < 1000; p++) {
+				arr[p] = Integer.parseInt(st.nextToken());
+				int check = arr[p];
+				++idxArr[check];
+			}
+			for (int p = 0; p < 1000; p++) {
+				System.out.printf(arr[p] + " ");
+			}
+			System.out.println();
+
+			int modNum = 0;
+			for (int p = 0; p <= 100; p++) {
+				if (idxArr[modNum] > idxArr[p]) {
+					modNum = p;
+				}
+			}
+			System.out.println("#" + caseNum + " " + modNum);
+		}
+	}
+}
