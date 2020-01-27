9fe4dba3c986f2d6b7f8a5ebbc9a8647aae4ecdb
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-01-27 15:30:38 +0900

    (sw) 조 만들기_(문제 해결 필요)

---
+++ src/WinterBase5/Solution_8104.java
@@ -0,0 +1,17 @@
+package WinterBase5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//조 만들기
+public class Solution_8104 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int testCase = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= testCase; i++) {
+
+		}
+	}
+}
