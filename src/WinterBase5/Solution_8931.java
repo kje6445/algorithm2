1ec1f06948dba75cad746f250d98312cdc40a4b6
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-01-31 23:09:51 +0900

    (sw) 제로_(문제 풀이 요함)

---
+++ src/WinterBase5/Solution_8931.java
@@ -0,0 +1,18 @@
+package WinterBase5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//제로
+public class Solution_8931 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int testCase = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= testCase; i++) {
+			System.out.println("#"+i);
+		}
+	}
+}
