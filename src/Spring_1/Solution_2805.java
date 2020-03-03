f7efc7021bb06bf9435409d4c88fb3baa0217629
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-03-03 11:19:18 +0900

    (sw) 농작물 수확하기
    - 위아래로 반으로 나눠서 열의 크기를 늘려가고 줄여가고 하는 것이였음
    - 하지만 난이도가 좀 있음...

--- src/Spring_1/Solution_2805.java
+++ src/Spring_1/Solution_2805.java
@@ -24,22 +24,23 @@ public class Solution_2805 {
 			int ans = 0;
 			int start = n/2;
 			for(int p=0; p<n; p++) {
+				//start기준 위은 크기를 키운다.
 				if(p<=start) {
 					int q = p*2+1;
 					int s = start-p;
 					while(q>0) {
 						ans = ans+map[p][s];
 						q = q-1;
-						s = s+1;
+						s = s+1; //열의 크기를 늘린다.
 					}
-				}
+				}//start기준 아래는 크기를 줄인다..
 				else {
 					int s = p-start;
 					int q = (start-s)*2+1;
 					while(q>0) {
 						ans = ans+map[p][s];
 						q = q-1;
-						s = s+1;
+						s = s+1; //열의 크기를 줄인다..
 					}
 				}
 			}
