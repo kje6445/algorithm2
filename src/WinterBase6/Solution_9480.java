51ae957e9bece7c294e623f86ede83d068957f4a
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-03-06 18:23:53 +0900

    (sw) 민정이와 광직이의 알파벳 공부(해결요함)
    -

---
+++ src/WinterBase6/Solution_9480.java
@@ -0,0 +1,21 @@
+package WinterBase6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//민정이와 광직이의 알파벳 공부
+public class Solution_9480 {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int testCase = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= testCase; i++) {
+			int N = Integer.parseInt(br.readLine());
+
+			for(int p=0; p<N; p++){
+				String word = br.readLine();
+
+			}
+		}
+	}
+}
