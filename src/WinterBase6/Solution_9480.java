85ba57349cc053c68fadf9a4435000cea2e7230d
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-03-15 22:12:02 +0900

    (sw) 정이와 광직이의 알파벳 공부(해결함)
    - 재귀적용법으로 문제를 해결
    - 몇 개의 단어 세트를 만들 수 있는지에대 다시 고민은 해볼것

--- src/WinterBase6/Solution_9480.java
+++ src/WinterBase6/Solution_9480.java
@@ -5,40 +5,50 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 //민정이와 광직이의 알파벳 공부
 public class Solution_9480 {
+	static int N;
+	static String array[];
+	static boolean check[];
+	static int result;
+
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		//단어 세트에 26개의 소문자 모두 포함되어야함
 		int testCase = Integer.parseInt(br.readLine());
 		for (int i = 1; i <= testCase; i++) {
-			int N = Integer.parseInt(br.readLine()); //민정이가 아는 단어의 개수 N
-			int count = 0; //민정이가 만들 수 있는 서로 다른 단어의 세트
-			int[] alphabet = new int[27];
-
-			for(int k=0; k<alphabet.length; k++){
-				alphabet[k] = 0;
+			N = Integer.parseInt(br.readLine()); //민정이가 아는 단어의 개수 N
+			array = new String[N];
+			check = new boolean[N];
+			result = 0;
+			for(int k =0; k<N; k++){
+				array[k] = br.readLine();
 			}
+			powerset(0);
+			System.out.println("#"+i+" "+ result);
+		}
+	}
 
-			for(int p=0; p<N; p++){
-				String word = br.readLine(); //민정이가 아는 단어 입력
-				char[] charArray = word.toCharArray();
-
-				for(int k=0; k< charArray.length; k++) {
-					char check = charArray[k];
-					int idx = (int)(check)- 97;
-					alphabet[idx]++;
+	static void powerset(int idx){
+		if(idx == array.length){
+			int alphabet[] = new int[26];
+			for(int i=0; i<N; i++){
+				if(check[i]){
+					for(int j=0; j<array[i].length(); j++){
+						alphabet[array[i].charAt(j) - 'a'] = 1;
+					}
 				}
 			}
-
-			int min = 0;
-			for(int k=0; k<alphabet.length; k++){
-				if(min>alphabet[k])
-					min = alphabet[k];
+			int alpha = 0;
+			for(int i=0; i<26; i++){ // 알파벳 배열이 0이면 alpha = 1
+				if(alphabet[i] == 0){
+					alpha = 1;
+				}
 			}
-
-			for(int k=0; k<alphabet.length; k++) {
-				System.out.println("alphabet[k] "+alphabet[k]);
-			}
-			System.out.println("#"+i+" "+min);
+			if(alpha == 0) result++; // result를 하나 올려준다.
+			return;
 		}
+		if(!check[idx]) check[idx] = true;
+		powerset(idx+1); //재귀적용법으로 문제 해결결
+		check[idx] = false;
+		powerset(idx+1);
 	}
 }
