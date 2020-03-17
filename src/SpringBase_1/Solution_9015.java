9206e255d60e357a7bc41a7426db8bebe03f564e
kje6445 <32263346+kje6445@users.noreply.github.com>
2020-03-17 21:44:55 +0900

    (sw) 배열의 분할(해결요함)
    - 세부적인 조건 달기

--- src/SpringBase_1/Solution_9015.java
+++ src/SpringBase_1/Solution_9015.java
@@ -3,6 +3,8 @@ package SpringBase_1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+
 //배열의 분할
 public class Solution_9015 {
 	public static void main(String args[]) throws IOException {
@@ -17,11 +19,21 @@ public class Solution_9015 {
 			for(int k=0; k<n; k++){
 				arr[k] = Integer.parseInt(numArr[k]);
 			}
-			int preNum;
+		//	int preNum;
+			Arrays.sort(arr);
 			int count = 0;
-			//다음 수가 증가
-			//다음 수가 감소
-
+			int check =0;
+			for(int k=0; k<n; k++) {
+				int preNum = arr[k];
+				//다음 수랑 값이 같을 경우
+				if(preNum == arr[k+1]){
+				//	++count;
+				}else if(preNum < arr[k+1]){//다음 수가 증가
+					check = 1;
+				}else{//다음 수가 감소
+					check = 2;
+				}
+			}
 
 			System.out.printf("#"+i+" "+count);
 		}
